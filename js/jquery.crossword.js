7debf149023d2d31b769a27aafdf98733fb06fa0
Jason <jasonbohio@gmail.com>
2024-07-15 10:32:32 -0400

    sn

--- js/jquery.crossword.js
+++ js/jquery.crossword.js
@@ -191,7 +191,7 @@
 						}
 
 						// while we're in here, add clues to DOM!
-						$('#' + puzz.data[i].orientation).append('<li tabindex="1" data-position="' + i + '">' + puzz.data[i].clue + '</li>'); 
+						$('#' + puzz.data[i].orientation).append('<ul tabindex="1" data-position="' + i + '">' + puzz.data[i].position + '. ' + puzz.data[i].clue + '</ul>'); 
 					}				
 					
 					// Calculate rows/cols by finding max coords of each entry, then picking the highest
