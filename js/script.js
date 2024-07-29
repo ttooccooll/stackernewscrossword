6923a66e72fc35cc3f8a3a0346064efad0ca0e83
Jason <jasonbohio@gmail.com>
2024-07-29 14:00:28 -0400

    blocksandstuff

--- js/script.js
+++ js/script.js
@@ -316,8 +316,10 @@
 	
 })(jQuery)
 
-let isToggled = false;
+let toggleState = 0;
 let usdPrice = null;
+let blockHeight = null;
+let satFee = null;
 
 async function fetchPrice() {
 	try {
@@ -329,15 +331,52 @@ async function fetchPrice() {
 	}
 }
 
+async function fetchBlock() {
+	try {
+		const response = await fetch('https://blockchain.info/q/getblockcount');
+		const data = await response.text();
+		blockHeight = parseInt(data).toFixed(0);
+	} catch (error) {
+		console.error('Error fetching the price:', error);
+	}
+}
+
+async function fetchFee() {
+	try {
+		const response = await fetch('https://mempool.space/api/v1/fees/recommended');
+		const data = await response.json();
+		satFee = data.halfHourFee.toFixed();
+		console.log(satFee);
+	} catch (error) {
+		console.error('Error fetching the price:', error);
+	}
+}
+
 async function togglePrice() {
 	if (!usdPrice) {
 		await fetchPrice();
 	}
-	const button = document.querySelector('.onesat');
-	if (isToggled) {
-		button.textContent = '1sat=1sat';
-	} else {
-		button.textContent = `$${usdPrice}`;
+	if (!blockHeight) {
+		await fetchBlock();
 	}
-	isToggled = !isToggled;
+	if (!satFee) {
+		await fetchFee();
+	}
+
+	const button = document.querySelector('.onesat');
+	switch (toggleState) {
+		case 0:
+			button.textContent = `${blockHeight}`;
+			break;
+		case 1:
+			button.textContent = `${satFee} sat/vB`;
+			break;
+		case 2:
+			button.textContent = `$${usdPrice}`;
+			break;
+		case 3:
+			button.textContent = '1sat=1sat';
+			break;
+	}
+	toggleState = (toggleState + 1) % 4;
 }
